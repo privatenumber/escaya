7b9eaa0e69e301ef7efbba075f59389858b52b33
Kenny F <kfrachelle123@gmail.com>
2021-01-31 10:55:27 +0800

    Update async_arrow_example.ts

--- async_arrow_example.ts
+++ async_arrow_example.ts
@@ -6,13 +6,21 @@ function parseCoverCallExpressionAndAsyncArrowHead(
   let pos = parser.pos;
   let trailingComma = false;
   let elements: any = [];
-  let state = Tristate.Unknown;
+  let state = Tristate.False;
 
   nextToken(parser, context | Context.AllowRegExp);
 
+  // Simple cases: "async () =>", "async (): ", and  "async () {".
+  if (parser.token === Token.RightParen) {
+    state = Tristate.True;
+  }
+
   while (parser.token & 0b00000110001001100101000000000000) {
     pos = parser.startPos;
-    if (parser.token & 0b00000000000010000101000000000000) {
+    if (parser.token & Token.IsAccessModifier && lookAhead(parser, context, nextTokenIsIdentifierOrPatternOnSameLine)) {
+      state = Tristate.True;
+      expression = parseFormalParameter(parser, context);
+    } else if (parser.token & 0b00000000000010000101000000000000) {
       if (context & Context.InYieldContext && parser.token === Token.YieldKeyword) {
         state = Tristate.False;
         expression = parseYieldExpression(parser, context, pos);
@@ -20,7 +28,7 @@ function parseCoverCallExpressionAndAsyncArrowHead(
         const token = parser.token;
         expression = parseBinaryExpression(parser, context, parseUnaryExpression(parser, context), 4, pos);
         if (expression.kind & (NodeKind.IsIdentifier | NodeKind.IsArrayOrObjectLiteral)) {
-          // If we have "(a?:" or "(a?," or "(a?=" or "(a?)" then this must be a optional
+          // If we have "async (a?:" or "async (a?," or "async (a?=" or "async (a?)" then this must be a optional
           // type-annotated parameter in an arrow function expression or a conditional expression
           // in an argument list.
           if (consumeOpt(parser, context, Token.QuestionMark)) {
@@ -38,21 +46,38 @@ function parseCoverCallExpressionAndAsyncArrowHead(
                   pos,
                   parser.startPos
                 );
+              } else {
+                state = Tristate.True;
+                expression = createFormalParameter(
+                  false,
+                  expression as any,
+                  true,
+                  null,
+                  parseInitializer(parser, context),
+                  null,
+                  null as any,
+                  NodeFlags.None,
+                  pos,
+                  parser.startPos
+                );
               }
             } else if (parser.token === Token.Colon) {
               state = Tristate.True;
               expression = createFormalParameter(
                 false,
-                expression,
+                expression as any,
                 true,
                 parseTypeAnnotation(parser, context),
                 parseInitializer(parser, context),
                 null,
-                null,
+                null as any,
                 NodeFlags.None,
                 pos,
                 parser.startPos
               );
+              // This has to be something like "async (a?"
+            } else {
+              state = Tristate.True;
             }
             // If we have something like "(a:", then we must have a
             // type-annotated parameter in an arrow function expression.
@@ -60,18 +85,18 @@ function parseCoverCallExpressionAndAsyncArrowHead(
             state = Tristate.True;
             expression = createFormalParameter(
               false,
-              expression,
+              expression as any,
               false,
               parseTypeAnnotation(parser, context),
               parseInitializer(parser, context),
               null,
-              null,
+              null as any,
               parser.nodeflags,
               pos,
               parser.startPos
             );
           } else {
-            // If we have "(a," or "(a=" or "(a)" this *could* be an arrow function
+            // If we have "async a," or "async (a=" or "async (a)" this *could* be an async arrow function
             state = Tristate.Unknown;
             if (parser.token & Token.IsAssignOp) {
               // If we have "(a %= b:" then this is definitely not an arrow function
@@ -81,7 +106,7 @@ function parseCoverCallExpressionAndAsyncArrowHead(
               expression = createAssignmentExpression(
                 expression,
                 operator,
-                parseAssignmentExpression(parser, context),
+                parseAssignmentExpression(parser, context) as any,
                 parser.nodeflags,
                 pos,
                 parser.startPos
@@ -100,10 +125,10 @@ function parseCoverCallExpressionAndAsyncArrowHead(
                 parser,
                 context,
                 createArrowParameters(
+                  null as any,
+                  [convertToSingleNameBinding(parseIdentifierReference(parser, context))],
                   null,
-                  parseIdentifierReference(parser, context),
-                  null,
-                  null,
+                  null as any,
                   /* trailingComma */ false,
                   parser.nodeflags,
                   pos,
@@ -121,7 +146,7 @@ function parseCoverCallExpressionAndAsyncArrowHead(
           nextToken(parser, context | Context.AllowRegExp);
           expression = createAssignmentExpression(
             expression,
-            operator,
+            operator as any,
             parseAssignmentExpression(parser, context),
             parser.nodeflags,
             pos,
@@ -135,38 +160,35 @@ function parseCoverCallExpressionAndAsyncArrowHead(
           expression = parseConditionalExpression(parser, context, expression, pos);
         }
 
-        if (expression.kind === NodeKind.ArrowParameters && parser.token === Token.Arrow) {
-          expression = parseArrowFunction(
-            parser,
-            context,
-            expression,
-            token === Token.AsyncKeyword,
-            /* isParenthesized */ true,
-            pos
-          );
-        }
-        // IdentifierReference [no LineTerminator here] `=>`
-        if (parser.token === Token.Arrow) {
-          expression = parseArrowFunction(
-            parser,
-            context,
-            createArrowParameters(
+        if (
+          parser.token === Token.Arrow &&
+          (token & (Token.FutureReserved | Token.IsIdentifier) || expression.kind === NodeKind.ArrowParameters)
+        ) {
+          const isParenthesized = expression.kind === NodeKind.ArrowParameters;
+          if (!isParenthesized) {
+            expression = createArrowParameters(
               createTypeParameters([], parser.nodeflags, pos, pos),
-              expression,
+              [convertToSingleNameBinding(expression)],
               null,
               null,
               /* trailingComma */ false,
               parser.nodeflags,
               pos,
               parser.startPos
-            ),
+            );
+          }
+          expression = parseArrowFunction(
+            parser,
+            context,
+            expression,
             token === Token.AsyncKeyword,
-            /* isParenthesized */ false,
+            isParenthesized,
             pos
           );
         }
       }
     } else if (parser.token & Token.IsEllipsis) {
+      state = Tristate.Unknown;
       nextToken(parser, context | Context.AllowRegExp);
       if (context & Context.InYieldContext && parser.token === Token.YieldKeyword) {
         state = Tristate.False;
@@ -195,12 +217,12 @@ function parseCoverCallExpressionAndAsyncArrowHead(
                 state = Tristate.True;
                 expression = createFormalParameter(
                   true,
-                  expression,
+                  expression as any,
                   true,
                   null,
                   parseInitializer(parser, context),
                   null,
-                  null,
+                  null as any,
                   NodeFlags.None,
                   pos,
                   parser.startPos
@@ -210,16 +232,19 @@ function parseCoverCallExpressionAndAsyncArrowHead(
               state = Tristate.True;
               expression = createFormalParameter(
                 true,
-                expression,
+                expression as any,
                 true,
                 parseTypeAnnotation(parser, context),
                 parseInitializer(parser, context),
                 null,
-                null,
+                null as any,
                 NodeFlags.None,
                 pos,
                 parser.startPos
               );
+              // This has to be something like "async (a?"
+            } else {
+              state = Tristate.True;
             }
             // If we have something like "(...a:" or "(...[a]?b:", then we must have a
             // type-annotated parameter in an arrow function expression.
@@ -228,12 +253,12 @@ function parseCoverCallExpressionAndAsyncArrowHead(
 
             expression = createFormalParameter(
               true,
-              expression,
+              expression as any,
               false,
               parseTypeAnnotation(parser, context),
               parseInitializer(parser, context),
               null,
-              null,
+              null as any,
               parser.nodeflags,
               pos,
               parser.startPos
@@ -251,7 +276,7 @@ function parseCoverCallExpressionAndAsyncArrowHead(
               expression = createAssignmentExpression(
                 expression,
                 operator,
-                parseAssignmentExpression(parser, context),
+                parseAssignmentExpression(parser, context) as any,
                 parser.nodeflags,
                 pos,
                 parser.startPos
@@ -278,25 +303,34 @@ function parseCoverCallExpressionAndAsyncArrowHead(
         }
 
         // IdentifierReference [no LineTerminator here] `=>`
-        if (parser.token === Token.Arrow) {
-          expression = parseArrowFunction(
-            parser,
-            context,
-            createArrowParameters(
+        if (
+          parser.token === Token.Arrow &&
+          (token & (Token.FutureReserved | Token.IsIdentifier) || expression.kind === NodeKind.ArrowParameters)
+        ) {
+          const isParenthesized = expression.kind === NodeKind.ArrowParameters;
+
+          if (!isParenthesized) {
+            expression = createArrowParameters(
               createTypeParameters([], parser.nodeflags, pos, pos),
-              expression,
+              [convertToSingleNameBinding(expression)],
               null,
               null,
               /* trailingComma */ false,
               parser.nodeflags,
               pos,
               parser.startPos
-            ),
+            );
+          }
+          expression = parseArrowFunction(
+            parser,
+            context,
+            expression,
             token === Token.AsyncKeyword,
-            /* isParenthesized */ false,
+            isParenthesized,
             pos
           );
         }
+
         expression = createSpreadElement(expression, parser.nodeflags, pos, parser.startPos);
       } else {
         state = Tristate.False;
@@ -310,6 +344,7 @@ function parseCoverCallExpressionAndAsyncArrowHead(
       // If we had "(" followed by something that's not an identifier, '...' or binding pattern,
       // then this definitely is an call expression.
     } else {
+      state = Tristate.False;
       expression =
         parser.token & Token.IsComma
           ? createOmittedExpression(NodeFlags.None, pos, pos)
@@ -351,10 +386,10 @@ function parseCoverCallExpressionAndAsyncArrowHead(
         }
 
         return createArrowParameters(
-          null,
+          null as any,
           elements,
           type,
-          null,
+          null as any,
           false,
           parser.nodeflags,
           pos,
